8227410d8c24e80bd3127e094c5e3e3c95df22f2
Aluno <Aluno@imac-de-administrador-10.local>
2018-11-08 17:58:50 -0300

    Initial Commit

---
+++ Urna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Urna
+//
+//  Created by Aluno on 08/11/2018.
+//  Copyright © 2018 Aluno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
